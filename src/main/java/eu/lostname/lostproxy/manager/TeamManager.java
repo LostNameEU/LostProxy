e2ebb72e46e8289a5a07d7d4aa250bf182a21d33
Nils K.-E <mebihon@gmx.de>
2020-12-08 15:52:27 +0100

    added JavaDoc for TeamManager.enableNotifications and TeamManager.disableNotifications
    Signed-off-by: Nils K.-E <mebihon@gmx.de>

--- src/main/java/eu/lostname/lostproxy/manager/TeamManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/TeamManager.java
@@ -13,11 +13,20 @@ public class TeamManager {
         this.notificationOn = new ArrayList<>();
     }
 
-
+    /**
+     * @param player   the player who gets the notifications enabled
+     * @param consumer a consumer which includes the feedback whether the adding to the list worked
+     * @since 08.12.2020
+     */
     public void enableNotifications(ProxiedPlayer player, Consumer<Boolean> consumer) {
         consumer.accept(notificationOn.add(player));
     }
 
+    /**
+     * @param player   the player who gets the notifications disabled
+     * @param consumer a consumer which includes the feedback whether the removing from the list worked
+     * @since 08.12.2020
+     */
     public void disableNotifications(ProxiedPlayer player, Consumer<Boolean> consumer) {
         consumer.accept(notificationOn.remove(player));
     }
