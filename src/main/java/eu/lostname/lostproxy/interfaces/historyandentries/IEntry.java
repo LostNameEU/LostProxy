fb75b0549606675a7bb9fb3a5feb010176531a36
realEntwickler <mebihon@gmx.de>
2020-12-10 23:05:44 +0100

    removed IEntry.compareTo cause this doenst work

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/IEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/IEntry.java
@@ -14,10 +14,6 @@ public class IEntry {
         this.timestamp = timestamp;
     }
 
-    public int compareTo(long timestamp2) {
-        return timestamp2 > timestamp ? 1 : 0;
-    }
-
     public String getUniqueId() {
         return _id;
     }
