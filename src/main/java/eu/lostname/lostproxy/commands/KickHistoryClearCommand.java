9ce007510adeed3207f75dbb35a8bca827f4b4de
realEntwickler <mebihon@gmx.de>
2020-12-11 17:11:06 +0100

    finished basic command when typing /khclear with a playername

--- src/main/java/eu/lostname/lostproxy/commands/KickHistoryClearCommand.java
+++ src/main/java/eu/lostname/lostproxy/commands/KickHistoryClearCommand.java
@@ -1,5 +1,6 @@
 package eu.lostname.lostproxy.commands;
 
+import eu.lostname.lostproxy.LostProxy;
 import eu.lostname.lostproxy.builder.MessageBuilder;
 import eu.lostname.lostproxy.utils.Prefix;
 import net.md_5.bungee.api.CommandSender;
@@ -19,7 +20,28 @@ public class KickHistoryClearCommand extends Command {
             commandSender.sendMessage(new MessageBuilder("§8┃ §c/khclear <Spieler> §8» §7Leert die Kick-History des angegebenen Spielers").addClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/khclear ").build());
             commandSender.sendMessage(new MessageBuilder("§8§m--------------------§r").build());
         } else if (strings.length == 1) {
+            LostProxy.getInstance().getPlayerManager().getUUIDofPlayername(strings[0], targetUUID -> {
+                if (targetUUID != null) {
+                    LostProxy.getInstance().getPlayerManager().getIPlayerAsync(targetUUID, targetiPlayer -> {
+                        LostProxy.getInstance().getHistoryManager().getKickHistory(targetUUID.toString(), iKickHistory -> {
+                            if (iKickHistory.getHistory().size() > 0) {
+                                commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "Möchtest du wirklich die §eKick-History §7von " + targetiPlayer.getPrefix() + targetiPlayer.getPlayerName() + " §clöschen§8?").build());
+                                commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "§7[§a§lKlick§7]").addClickEvent(ClickEvent.Action.RUN_COMMAND, "/khclear " + strings[0] + " confirmed").build());
+                            } else {
+                                commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "Die §eKick-History §7von " + targetiPlayer.getPrefix() + targetiPlayer.getPlayerName() + " §7ist §cleer§8.").build());
+                            }
+                        });
+                    });
+                } else {
+                    commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "Der angegebene Spieler konnte §cnicht §7gefunden werden§8.").build());
+                }
+            });
+        } else if (strings.length == 2) {
+            if (strings[1].equalsIgnoreCase("confirmed")) {
 
+            } else {
+                commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "Bitte beachte die §eBenutzung §7dieses Kommandos§8.").build());
+            }
         } else {
             commandSender.sendMessage(new MessageBuilder(Prefix.BKMS + "Bitte beachte die §eBenutzung §7dieses Kommandos§8.").build());
         }
