f443ad3e4dfec92da99874fa43d4460511031003
realEntwickler <mebihon@gmx.de>
2020-12-12 15:58:38 +0100

    added function to send message to other online team members when some team member left the ingame network

--- src/main/java/eu/lostname/lostproxy/listener/PlayerDisconnectListener.java
+++ src/main/java/eu/lostname/lostproxy/listener/PlayerDisconnectListener.java
@@ -1,6 +1,8 @@
 package eu.lostname.lostproxy.listener;
 
 import eu.lostname.lostproxy.LostProxy;
+import eu.lostname.lostproxy.builder.MessageBuilder;
+import eu.lostname.lostproxy.utils.Prefix;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -14,6 +16,17 @@ public class PlayerDisconnectListener implements Listener {
 
         if (player.hasPermission("lostproxy.notify")) {
             LostProxy.getInstance().getTeamManager().disableNotifications(player, aBoolean -> {
+            });
+        }
+
+        if (player.hasPermission("lostproxy.command.team") && player.hasPermission("lostproxy.command.team.logout")) {
+            LostProxy.getInstance().getTeamManager().logout(player, aBoolean -> {
+                if (aBoolean)
+                    LostProxy.getInstance().getPlayerManager().getIPlayerAsync(player.getUniqueId(), iPlayer -> {
+                        LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> {
+                            all.sendMessage(new MessageBuilder(Prefix.TEAM + iPlayer.getPrefix() + iPlayer.getPlayerName() + " §7hat das Netzwerk §cverlassen§8.").build());
+                        });
+                    });
             });
         }
     }
