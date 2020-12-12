6bfbe0d92f68f4085c4790ea4f4d05767c70bc7c
realEntwickler <mebihon@gmx.de>
2020-12-12 21:19:03 +0100

    added missing permission checks

--- src/main/java/eu/lostname/lostproxy/commands/TeamCommand.java
+++ src/main/java/eu/lostname/lostproxy/commands/TeamCommand.java
@@ -29,35 +29,47 @@ public class TeamCommand extends Command {
                 LostProxy.getInstance().getPlayerManager().getIPlayerAsync(player.getUniqueId(), iPlayer -> {
                     switch (strings[0]) {
                         case "login":
-                            if (!LostProxy.getInstance().getTeamManager().isLoggedIn(player)) {
-                                LostProxy.getInstance().getTeamManager().login(player, aBoolean -> {
-                                    if (aBoolean) {
-                                        LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> all.sendMessage(new MessageBuilder(Prefix.TMS + iPlayer.getPrefix() + player.getName() + " §7hat sich §aeingeloggt§8.").build()));
-                                    } else {
-                                        player.sendMessage(new MessageBuilder(Prefix.TMS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
-                                    }
-                                });
+                            if (player.hasPermission("lostproxy.command.team.login")) {
+                                if (!LostProxy.getInstance().getTeamManager().isLoggedIn(player)) {
+                                    LostProxy.getInstance().getTeamManager().login(player, aBoolean -> {
+                                        if (aBoolean) {
+                                            LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> all.sendMessage(new MessageBuilder(Prefix.TMS + iPlayer.getPrefix() + player.getName() + " §7hat sich §aeingeloggt§8.").build()));
+                                        } else {
+                                            player.sendMessage(new MessageBuilder(Prefix.TMS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
+                                        }
+                                    });
+                                } else {
+                                    player.sendMessage(new MessageBuilder(Prefix.TMS + "Du bist §cbereits §7eingeloggt§8.").build());
+                                }
                             } else {
-                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Du bist §cbereits §7eingeloggt§8.").build());
+                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Du hast §cnicht §7die erforderlichen Rechte§8, §7um dieses Kommando auszuführen§8.").build());
                             }
                             break;
                         case "logout":
-                            if (LostProxy.getInstance().getTeamManager().isLoggedIn(player)) {
-                                LostProxy.getInstance().getTeamManager().logout(player, aBoolean -> {
-                                    if (aBoolean) {
-                                        player.sendMessage(new MessageBuilder(Prefix.TMS + "Du wurdest §causgeloggt§8.").build());
-                                        LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> all.sendMessage(new MessageBuilder(Prefix.TMS + iPlayer.getPrefix() + player.getName() + " §7hat sich §causgeloggt§8.").build()));
-                                    } else {
-                                        player.sendMessage(new MessageBuilder(Prefix.TMS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
-                                    }
-                                });
+                            if (player.hasPermission("lostproxy.command.team.logout")) {
+                                if (LostProxy.getInstance().getTeamManager().isLoggedIn(player)) {
+                                    LostProxy.getInstance().getTeamManager().logout(player, aBoolean -> {
+                                        if (aBoolean) {
+                                            player.sendMessage(new MessageBuilder(Prefix.TMS + "Du wurdest §causgeloggt§8.").build());
+                                            LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> all.sendMessage(new MessageBuilder(Prefix.TMS + iPlayer.getPrefix() + player.getName() + " §7hat sich §causgeloggt§8.").build()));
+                                        } else {
+                                            player.sendMessage(new MessageBuilder(Prefix.TMS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
+                                        }
+                                    });
+                                } else {
+                                    player.sendMessage(new MessageBuilder(Prefix.TMS + "Du bist §cnicht §7eingeloggt§8.").build());
+                                }
                             } else {
-                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Du bist §cnicht §7eingeloggt§8.").build());
+                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Du hast §cnicht §7die erforderlichen Rechte§8, §7um dieses Kommando auszuführen§8.").build());
                             }
                             break;
                         case "list":
-                            player.sendMessage(new MessageBuilder(Prefix.TMS + "Übersicht der Teammitglieder§8:").build());
-                            ProxyServer.getInstance().getPlayers().stream().filter(filter -> filter.hasPermission("lostproxy.command.team")).forEach(all -> LostProxy.getInstance().getPlayerManager().getIPlayerAsync(all.getUniqueId(), allIPlayer -> player.sendMessage(new MessageBuilder("§8┃ " + allIPlayer.getPrefix() + allIPlayer.getPlayerName() + " §8» " + (LostProxy.getInstance().getTeamManager().isLoggedIn(all) ? "§a✔" : "§c✖") + " §8» §7" + all.getServer().getInfo().getName()).build())));
+                            if (player.hasPermission("lostproxy.command.team.list")) {
+                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Übersicht der Teammitglieder§8:").build());
+                                ProxyServer.getInstance().getPlayers().stream().filter(filter -> filter.hasPermission("lostproxy.command.team")).forEach(all -> LostProxy.getInstance().getPlayerManager().getIPlayerAsync(all.getUniqueId(), allIPlayer -> player.sendMessage(new MessageBuilder("§8┃ " + allIPlayer.getPrefix() + allIPlayer.getPlayerName() + " §8» " + (LostProxy.getInstance().getTeamManager().isLoggedIn(all) ? "§a✔" : "§c✖") + " §8» §7" + all.getServer().getInfo().getName()).build())));
+                            } else {
+                                player.sendMessage(new MessageBuilder(Prefix.TMS + "Du hast §cnicht §7die erforderlichen Rechte§8, §7um dieses Kommando auszuführen§8.").build());
+                            }
                             break;
                         default:
                             player.sendMessage(new MessageBuilder(Prefix.TMS + "Bitte beachte die §eBenutzung §7dieses Kommandos§8.").build());
