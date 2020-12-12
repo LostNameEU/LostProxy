7cff9b54d087d301f661bba2bc1072c152da1eb6
realEntwickler <mebihon@gmx.de>
2020-12-12 23:41:27 +0100

    added methods for getting and saving ban histories

--- src/main/java/eu/lostname/lostproxy/manager/HistoryManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/HistoryManager.java
@@ -3,6 +3,8 @@ package eu.lostname.lostproxy.manager;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ReplaceOptions;
 import eu.lostname.lostproxy.LostProxy;
+import eu.lostname.lostproxy.interfaces.historyandentries.ban.IBanEntry;
+import eu.lostname.lostproxy.interfaces.historyandentries.ban.IBanHistory;
 import eu.lostname.lostproxy.interfaces.historyandentries.kick.IKickEntry;
 import eu.lostname.lostproxy.interfaces.historyandentries.kick.IKickHistory;
 import eu.lostname.lostproxy.utils.MongoCollection;
@@ -37,6 +39,35 @@ public class HistoryManager {
     public void saveKickHistory(String uniqueId, IKickHistory iKickHistory, Consumer<Boolean> consumer) {
         LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.KICK_HISTORIES).replaceOne(Filters.eq("_id", uniqueId),
                 LostProxy.getInstance().getGson().fromJson(LostProxy.getInstance().getGson().toJson(iKickHistory), Document.class),
+                new ReplaceOptions().upsert(true),
+                (updateResult, throwable) -> {
+                    if (updateResult.wasAcknowledged()) {
+                        consumer.accept(true);
+                    } else {
+                        consumer.accept(false);
+                        throwable.printStackTrace(System.out);
+                    }
+                });
+    }
+
+    public void getBanHistory(String uniqueId, Consumer<IKickHistory> consumer) {
+        LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.BAN_HISTORIES).find(Filters.eq("_id", uniqueId)).first((document, throwable) -> {
+            if (document == null) {
+                IBanHistory iBanHistory = new IBanHistory(uniqueId, new ArrayList<IBanEntry>());
+                document = LostProxy.getInstance().getGson().fromJson(LostProxy.getInstance().getGson().toJson(iBanHistory), Document.class);
+
+                LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.BAN_HISTORIES).insertOne(document, (unused, throwable1) -> {
+                    throwable1.printStackTrace(System.out);
+                });
+            }
+
+            consumer.accept(LostProxy.getInstance().getGson().fromJson(document.toJson(), IKickHistory.class));
+        });
+    }
+
+    public void saveBanHistory(String uniqueId, IBanHistory iBanHistory, Consumer<Boolean> consumer) {
+        LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.BAN_HISTORIES).replaceOne(Filters.eq("_id", uniqueId),
+                LostProxy.getInstance().getGson().fromJson(LostProxy.getInstance().getGson().toJson(iBanHistory), Document.class),
                 new ReplaceOptions().upsert(true),
                 (updateResult, throwable) -> {
                     if (updateResult.wasAcknowledged()) {
