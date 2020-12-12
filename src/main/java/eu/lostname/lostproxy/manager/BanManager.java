0c63781bd57fb3ac58e92d13648f4311c662d342
realEntwickler <mebihon@gmx.de>
2020-12-13 00:07:14 +0100

    added javadocs

--- src/main/java/eu/lostname/lostproxy/manager/BanManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/BanManager.java
@@ -11,6 +11,11 @@ import java.util.function.Consumer;
 
 public class BanManager {
 
+
+    /**
+     * @param uniqueId the uniqueId of the player who has to be checked
+     * @param consumer returns the iban when ban is active
+     */
     public void getBan(String uniqueId, Consumer<IBan> consumer) {
         LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.ACTIVE_BANS).find(Filters.eq("_id", uniqueId)).first((document, throwable) -> {
             if (document != null) {
@@ -21,6 +26,10 @@ public class BanManager {
         });
     }
 
+    /**
+     * @param iBan                     that is gonna be inserted into the database
+     * @param voidSingleResultCallback returns the callback from the database
+     */
     public void insertBan(IBan iBan, SingleResultCallback<Void> voidSingleResultCallback) {
         LostProxy.getInstance().getDatabase().getMongoDatabase().getCollection(MongoCollection.ACTIVE_BANS).insertOne(LostProxy.getInstance().getGson().fromJson(LostProxy.getInstance().getGson().toJson(iBan), Document.class), voidSingleResultCallback);
     }
