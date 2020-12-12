96df1fe822f014d7aa29c516767a6ac9cb6fa5a0
realEntwickler <mebihon@gmx.de>
2020-12-12 19:48:57 +0100

    fixed another wrong prefix

--- src/main/java/eu/lostname/lostproxy/listener/PostLoginListener.java
+++ src/main/java/eu/lostname/lostproxy/listener/PostLoginListener.java
@@ -31,7 +31,7 @@ public class PostLoginListener implements Listener {
 
                     LostProxy.getInstance().getPlayerManager().getIPlayerAsync(player.getUniqueId(), iPlayer -> LostProxy.getInstance().getTeamManager().getLoggedIn().forEach(all -> all.sendMessage(new MessageBuilder(Prefix.TMS + iPlayer.getPrefix() + iPlayer.getPlayerName() + " §7hat das Netzwerk §abetreten§8.").build())));
                 } else {
-                    player.sendMessage(new MessageBuilder(Prefix.NOTIFICATIONS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
+                    player.sendMessage(new MessageBuilder(Prefix.TMS + "§7Es ist ein §4Fehler §7aufgetreten§8. §7Bitte kontaktiere sofort das Referat §4DEV/01§8!").build());
                 }
             });
         }
