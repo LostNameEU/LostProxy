6e8aaa793af629f851da0237f49a633247fdaa2d
realEntwickler <mebihon@gmx.de>
2020-12-13 21:46:56 +0100

    added MongoCollection.BAN_REASONS

--- src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
+++ src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
@@ -5,4 +5,5 @@ public class MongoCollection {
     public static final String KICK_HISTORIES = "kickHistories";
     public static final String BAN_HISTORIES = "banHistories";
     public static final String ACTIVE_BANS = "activeBans";
+    public static final String BAN_REASONS = "banReasons";
 }
