40c1103481f5919173fddec7c603f0c53cfd67a5
realEntwickler <mebihon@gmx.de>
2020-12-13 17:40:18 +0100

    added IBanSpecificEntry.reason

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanSpecificEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanSpecificEntry.java
@@ -5,10 +5,16 @@ import eu.lostname.lostproxy.interfaces.historyandentries.IEntry;
 public class IBanSpecificEntry extends IEntry {
 
     private final IBanEntryType iBanEntryType;
+    private final String reason;
 
-    public IBanSpecificEntry(String _id, String invokerId, long timestamp, IBanEntryType iBanEntryType) {
+    public IBanSpecificEntry(String _id, String invokerId, long timestamp, String reason, IBanEntryType iBanEntryType) {
         super(_id, invokerId, timestamp);
+        this.reason = reason;
         this.iBanEntryType = iBanEntryType;
+    }
+
+    public String getReason() {
+        return reason;
     }
 
     public IBanEntryType getIBanEntryType() {
