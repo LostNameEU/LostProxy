c742e2376cdd7c9a2904f0f0d80176df2fafac12
realEntwickler <mebihon@gmx.de>
2020-12-13 17:19:16 +0100

    rewrited IEntry.java

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/IEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/IEntry.java
@@ -4,13 +4,11 @@ public class IEntry {
 
     private final String _id;
     private final String invokerId;
-    private final String reason;
     private final long timestamp;
 
-    public IEntry(String _id, String invokerId, String reason, long timestamp) {
+    public IEntry(String _id, String invokerId, long timestamp) {
         this._id = _id;
         this.invokerId = invokerId;
-        this.reason = reason;
         this.timestamp = timestamp;
     }
 
@@ -24,10 +22,6 @@ public class IEntry {
 
     public boolean isInvokerConsole() {
         return invokerId.equalsIgnoreCase("console");
-    }
-
-    public String getReason() {
-        return reason;
     }
 
     public long getTimestamp() {
