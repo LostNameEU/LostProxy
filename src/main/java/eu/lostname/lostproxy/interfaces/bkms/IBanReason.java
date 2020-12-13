e7700b78458f92eae1fa0ddc3aa3a53b8dbbd39b
realEntwickler <mebihon@gmx.de>
2020-12-13 21:38:17 +0100

    created IBanReason

---
+++ src/main/java/eu/lostname/lostproxy/interfaces/bkms/IBanReason.java
@@ -0,0 +1,44 @@
+package eu.lostname.lostproxy.interfaces.bkms;
+
+import eu.lostname.lostproxy.enums.EReasonType;
+import eu.lostname.lostproxy.interfaces.IReason;
+
+import java.util.concurrent.TimeUnit;
+
+public class IBanReason extends IReason {
+
+    private long time;
+    private TimeUnit timeUnit;
+    private String permission;
+
+    public IBanReason(int _id, String name, long time, TimeUnit timeUnit, String permission) {
+        super(_id, name, EReasonType.BAN);
+        this.time = time;
+        this.timeUnit = timeUnit;
+        this.permission = permission;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+}
