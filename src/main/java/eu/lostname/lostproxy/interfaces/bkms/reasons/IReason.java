365649ff96a4f6df97a3a9876ac935f8cadd82e6
realEntwickler <mebihon@gmx.de>
2020-12-13 21:28:03 +0100

    added IReason.timeUnit added getter and setter for new parameter

--- src/main/java/eu/lostname/lostproxy/interfaces/bkms/reasons/IReason.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/bkms/reasons/IReason.java
@@ -1,16 +1,20 @@
 package eu.lostname.lostproxy.interfaces.bkms.reasons;
 
+import java.util.concurrent.TimeUnit;
+
 public class IReason {
 
     private int _id;
     private String name;
-    private long duration;
+    private long time;
+    private TimeUnit timeUnit;
     private String permission;
 
-    public IReason(int _id, String name, long duration, String permission) {
+    public IReason(int _id, String name, long time, TimeUnit timeUnit, String permission) {
         this._id = _id;
         this.name = name;
-        this.duration = duration;
+        this.time = time;
+        this.timeUnit = timeUnit;
         this.permission = permission;
     }
 
@@ -30,12 +34,12 @@ public class IReason {
         this.name = name;
     }
 
-    public long getDuration() {
-        return duration;
+    public long getTime() {
+        return time;
     }
 
-    public void setDuration(long duration) {
-        this.duration = duration;
+    public void setTime(long time) {
+        this.time = time;
     }
 
     public String getPermission() {
@@ -44,5 +48,13 @@ public class IReason {
 
     public void setPermission(String permission) {
         this.permission = permission;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
     }
 }
