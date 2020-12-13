e47256a984a83a0e910a225245619fa72df3c7eb
realEntwickler <mebihon@gmx.de>
2020-12-13 17:41:39 +0100

    refactored IUnbanEntry.java due to new IBanSpecificEntry.java

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IUnbanEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IUnbanEntry.java
@@ -2,14 +2,7 @@ package eu.lostname.lostproxy.interfaces.historyandentries.ban;
 
 public class IUnbanEntry extends IBanSpecificEntry {
 
-    private final String reason;
-
     public IUnbanEntry(String _id, String invokerId, long timestamp, String reason) {
-        super(_id, invokerId, timestamp, IBanEntryType.UNBAN_ENTRY);
-        this.reason = reason;
-    }
-
-    public String getReason() {
-        return reason;
+        super(_id, invokerId, timestamp, reason, IBanEntryType.UNBAN_ENTRY);
     }
 }
