1585a9ea0681e8154557bfec183dfd2b9f7840c7
realEntwickler <mebihon@gmx.de>
2020-12-13 17:46:13 +0100

    refactored IBanEntry.java

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanEntry.java
@@ -2,19 +2,13 @@ package eu.lostname.lostproxy.interfaces.historyandentries.ban;
 
 public class IBanEntry extends IBanSpecificEntry {
 
-    private final String reason;
     private final long duration;
     private final long end;
 
     public IBanEntry(String _id, String invokerId, long timestamp, IBanEntryType iBanEntryType, String reason, long duration, long end) {
-        super(_id, invokerId, timestamp, iBanEntryType);
-        this.reason = reason;
+        super(_id, invokerId, timestamp, reason, iBanEntryType);
         this.duration = duration;
         this.end = end;
-    }
-
-    public String getReason() {
-        return reason;
     }
 
     public long getDuration() {
