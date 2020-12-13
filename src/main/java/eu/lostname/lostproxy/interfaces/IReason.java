b0be07c379bee7540d36f6d12521bf2fde00f86e
realEntwickler <mebihon@gmx.de>
2020-12-13 21:35:40 +0100

    created IReason.java for ban, mute or report reasons

---
+++ src/main/java/eu/lostname/lostproxy/interfaces/IReason.java
@@ -0,0 +1,39 @@
+package eu.lostname.lostproxy.interfaces;
+
+
+public class IReason {
+
+    private int _id;
+    private String name;
+    private EReasonType eReasonType;
+
+    public IReason(int _id, String name, EReasonType eReasonType) {
+        this._id = _id;
+        this.name = name;
+        this.eReasonType = eReasonType;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EReasonType getReasonType() {
+        return eReasonType;
+    }
+
+    public void setReasonType(EReasonType eReasonType) {
+        this.eReasonType = eReasonType;
+    }
+}
