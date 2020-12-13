3b7ce365a604769d1ef7ba508b2e708b76457757
realEntwickler <mebihon@gmx.de>
2020-12-13 21:34:36 +0100

    removed EBanEntryType from constructor and set it to EBanEntryType.BAN_APPEAL_ENTRY

--- src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanEntry.java
+++ src/main/java/eu/lostname/lostproxy/interfaces/historyandentries/ban/IBanEntry.java
@@ -1,12 +1,14 @@
 package eu.lostname.lostproxy.interfaces.historyandentries.ban;
 
+import eu.lostname.lostproxy.enums.EBanEntryType;
+
 public class IBanEntry extends IBanSpecificEntry {
 
     private final long duration;
     private final long end;
 
-    public IBanEntry(String _id, String invokerId, long timestamp, IBanEntryType iBanEntryType, String reason, long duration, long end) {
-        super(_id, invokerId, timestamp, reason, iBanEntryType);
+    public IBanEntry(String _id, String invokerId, long timestamp, String reason, long duration, long end) {
+        super(_id, invokerId, timestamp, reason, EBanEntryType.BAN_ENTRY);
         this.duration = duration;
         this.end = end;
     }
