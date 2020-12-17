85c4b220e2088c9e98670e95c5393b80f95aa182
realEntwickler <mebihon@gmx.de>
2020-12-17 21:16:22 +0100

    add MongoCollection.MUTE_HISTORIES

--- src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
+++ src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
@@ -6,4 +6,5 @@ public class MongoCollection {
     public static final String BAN_HISTORIES = "banHistories";
     public static final String ACTIVE_BANS = "activeBans";
     public static final String BAN_REASONS = "banReasons";
+    public static final String MUTE_HISTORIES = "muteHistories";
 }
