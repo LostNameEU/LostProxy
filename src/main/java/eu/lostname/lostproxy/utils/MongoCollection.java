ab7bbfb969d50d0e05ee581c533b1fd9d39ec2bf
realEntwickler <mebihon@gmx.de>
2020-12-17 21:17:11 +0100

    add MongoCollection.ACTIVE_MUTES

--- src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
+++ src/main/java/eu/lostname/lostproxy/utils/MongoCollection.java
@@ -7,4 +7,5 @@ public class MongoCollection {
     public static final String ACTIVE_BANS = "activeBans";
     public static final String BAN_REASONS = "banReasons";
     public static final String MUTE_HISTORIES = "muteHistories";
+    public static final String ACTIVE_MUTES = "activeMutes";
 }
