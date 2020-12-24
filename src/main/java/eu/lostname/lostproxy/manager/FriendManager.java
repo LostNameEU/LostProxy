ed873a23a145a83e9929382a2e6e5725b5137950
realEntwickler <mebihon@gmx.de>
2020-12-24 19:47:08 +0100

    added FriendManager.getFriendData

--- src/main/java/eu/lostname/lostproxy/manager/FriendManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/FriendManager.java
@@ -1,7 +1,14 @@
 package eu.lostname.lostproxy.manager;
 
 import com.google.gson.Gson;
+import com.mongodb.client.model.Filters;
 import eu.lostname.lostproxy.databases.LostProxyDatabase;
+import eu.lostname.lostproxy.interfaces.IFriendData;
+import eu.lostname.lostproxy.utils.MongoCollection;
+import org.bson.Document;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class FriendManager {
 
@@ -13,5 +20,15 @@ public class FriendManager {
         this.gson = gson;
     }
 
+    public IFriendData getFriendData(UUID uniqueId) {
+        Document d = database.getMongoDatabase().getCollection(MongoCollection.FRIEND_DATA).find(Filters.eq("_id", uniqueId.toString())).first();
 
+        if (d == null) {
+            d = gson.fromJson(gson.toJson(new IFriendData(uniqueId.toString(), System.currentTimeMillis(), new HashMap<>(), new HashMap<>(), true, true, true, true)), Document.class);
+
+            database.getMongoDatabase().getCollection(MongoCollection.FRIEND_DATA).insertOne(d);
+        }
+
+        return gson.fromJson(d.toJson(), IFriendData.class);
+    }
 }
