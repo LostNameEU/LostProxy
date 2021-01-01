7c9a13927ce1372fa5b6189e3a468d067e22179f
realEntwickler <mebihon@gmx.de>
2021-01-01 23:35:32 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/databases/LostProxyDatabase.java
+++ src/main/java/eu/lostname/lostproxy/databases/LostProxyDatabase.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:35:32
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * LostProxyDatabase.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
 package eu.lostname.lostproxy.databases;
 
 import eu.lostname.lostproxy.interfaces.IDatabaseConnection;
