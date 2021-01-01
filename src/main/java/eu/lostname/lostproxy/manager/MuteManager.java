bb21d8d404d242b62898da837eac4f91ecc258de
realEntwickler <mebihon@gmx.de>
2021-01-01 23:41:04 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/manager/MuteManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/MuteManager.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:41:03
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * MuteManager.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
 package eu.lostname.lostproxy.manager;
 
 import com.google.gson.Gson;
