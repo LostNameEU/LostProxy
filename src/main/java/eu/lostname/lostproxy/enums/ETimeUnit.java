defac1c27344aabbecb04945426175441031f316
realEntwickler <mebihon@gmx.de>
2021-01-01 23:35:55 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/enums/ETimeUnit.java
+++ src/main/java/eu/lostname/lostproxy/enums/ETimeUnit.java
@@ -1,4 +1,14 @@
 /*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:35:54
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * ETimeUnit.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
+/*
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
  *
