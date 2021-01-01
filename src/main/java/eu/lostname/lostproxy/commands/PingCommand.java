bd3d377e13b58336bed0e2153fab8ab9b1fcfd3c
realEntwickler <mebihon@gmx.de>
2021-01-01 23:34:53 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/commands/PingCommand.java
+++ src/main/java/eu/lostname/lostproxy/commands/PingCommand.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:34:53
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * PingCommand.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
 package eu.lostname.lostproxy.commands;
 
 import eu.lostname.lostproxy.builder.MessageBuilder;
@@ -37,7 +47,7 @@ public class PingCommand extends Command implements TabExecutor {
                     if (target != null) {
                         ProxiedPlayer finalTarget = target;
                         IPlayerSync iPlayer = new IPlayerSync(target.getUniqueId());
-                            player.sendMessage(new MessageBuilder(Prefix.LOSTPROXY + "Der Ping von " + iPlayer.getDisplay() + finalTarget.getName() + " §7beträgt aktuell§8: §c" + finalTarget.getPing() + "ms").build());
+                        player.sendMessage(new MessageBuilder(Prefix.LOSTPROXY + "Der Ping von " + iPlayer.getDisplay() + finalTarget.getName() + " §7beträgt aktuell§8: §c" + finalTarget.getPing() + "ms").build());
                     } else {
                         player.sendMessage(new MessageBuilder(Prefix.LOSTPROXY + "Der angegebene Spieler konnte §cnicht §7gefunden werden§8.").build());
                     }
