44196c476d29eaaa69a1db4fc320214df24b8be3
realEntwickler <mebihon@gmx.de>
2021-01-01 23:41:20 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/manager/PlayerManager.java
+++ src/main/java/eu/lostname/lostproxy/manager/PlayerManager.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:41:20
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * PlayerManager.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
 package eu.lostname.lostproxy.manager;
 
 import de.dytanic.cloudnet.driver.CloudNetDriver;
