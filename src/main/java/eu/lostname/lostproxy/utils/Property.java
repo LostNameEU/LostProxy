b581c8e9682e2c86fa628c80e28688d4a41ac6b8
realEntwickler <mebihon@gmx.de>
2021-01-01 23:42:03 +0100

    added Copyright

--- src/main/java/eu/lostname/lostproxy/utils/Property.java
+++ src/main/java/eu/lostname/lostproxy/utils/Property.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright notice
+ * Copyright (c) Nils Körting-Eberhardt 2021
+ * Created: 01.01.2021 @ 23:42:02
+ *
+ * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
+ *
+ * Property.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ */
+
 package eu.lostname.lostproxy.utils;
 
 import java.io.*;
