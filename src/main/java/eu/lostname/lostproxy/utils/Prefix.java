7392963af7e75ea68811e6edb9ac74973d6f2292
realEntwickler <mebihon@gmx.de>
2021-01-02 23:20:17 +0100

    updated prefixes

--- src/main/java/eu/lostname/lostproxy/utils/Prefix.java
+++ src/main/java/eu/lostname/lostproxy/utils/Prefix.java
@@ -1,22 +1,22 @@
 /*
  * Copyright notice
  * Copyright (c) Nils Körting-Eberhardt 2021
- * Created: 01.01.2021 @ 23:41:52
+ * Created: 02.01.2021 @ 23:20:16
  *
  * All contents of this source code are protected by copyright. The copyright is owned by Nils Körting-Eberhardt, unless explicitly stated otherwise. All rights reserved.
  *
- * Prefix.java is part of the lostproxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
+ * Prefix.java is part of the LostProxy which is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.
  */
 
 package eu.lostname.lostproxy.utils;
 
 public class Prefix {
 
-    public static final String LOSTPROXY = "§8┃ §cLostProxy §8» §7";
-    public static final String NOTIFICATIONS = "§8┃ §bNotifications §8» §7";
-    public static final String MAINTENANCE = "§8┃ §4Maintenance §8» §7";
-    public static final String BKMS = "§8┃ §cBKM-System §8» §7";
-    public static final String TEAMSPEAK = "§8┃ §bTeamSpeak §8» §7";
-    public static final String TMS = "§8┃ §aTM-System §8» §7";
-    public static final String FRIENDS = "§8┃ §cFreunde §8» §7";
+    public static final String LOSTPROXY = "§8┃ §c§lLostProxy §8» §7";
+    public static final String NOTIFICATIONS = "§8┃ §b§lNotify §8» §7";
+    public static final String MAINTENANCE = "§8┃ §4§lMaintenance §8» §7";
+    public static final String BKMS = "§8┃ §c§lBKMS §8» §7";
+    public static final String TEAMSPEAK = "§8┃ §b§lTeamSpeak §8» §7";
+    public static final String TMS = "§8┃ §a§lTMS §8» §7";
+    public static final String FRIENDS = "§8┃ §c§lFreunde §8» §7";
 }
